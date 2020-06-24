a2f7ba12dbd1419f5a2d129a899f55c4f6304a6e
Neeraj Jain <neeraj_jain@apple.com>
2020-06-24 16:54:54 +0530

    Added DevTools LiveReload and Jmeter Load Testing Script

--- src/main/java/com/neeraj/loadbalancing/MyLoadBalancingApplication.java
+++ src/main/java/com/neeraj/loadbalancing/MyLoadBalancingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoadbalancingApplication {
+public class MyLoadBalancingApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(LoadbalancingApplication.class, args);
+		SpringApplication.run(MyLoadBalancingApplication.class, args);
 	}
 }
