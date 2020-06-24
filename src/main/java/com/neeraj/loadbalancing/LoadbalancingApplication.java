9673033943e62b9cc37b7d7cc2dc14eb16347373
Neeraj Jain <neeraj_jain@apple.com>
2020-06-24 12:34:32 +0530

    Initial Commit

---
+++ src/main/java/com/neeraj/loadbalancing/LoadbalancingApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.loadbalancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neeraj/loadbalancing/LoadbalancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.loadbalancing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
